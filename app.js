c39350e6223c7dd09c1b5e03cd1b21852cda14f4
Anthony Kumasaka <akumasaka@courtbuddy.com>
2019-05-16 17:05:36 -0700

    Start user auth

--- app.js
+++ app.js
@@ -2,13 +2,26 @@ const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
 const db = require('./config/keys').mongoURI;
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const passport = require('passport'); 
+const bodyParser = require('body-parser');
+// const User = require('./models/User'); 
 
 mongoose
     .connect(db, { useNewUrlParser: true })
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
-    
-app.get("/", (req, res) => res.send("Hello Court Buddy aKuma"));
+
+app.use(passport.initialize()); 
+require('./config/passport')(passport);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
 
 const port = process.env.PORT || 5000;
 
---
+++ frontend/src/components/main/main_page.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Court Buddy</h1>
+                <footer>
+                    Copyright &copy; 2019 Court Buddy
+        </footer>
+            </div>
+        );
+    }
+}
+
+export default MainPage;
